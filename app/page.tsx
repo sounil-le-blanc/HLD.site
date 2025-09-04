3c7fd063f486cc53630c22ed6ad0942aab1c9768
XERXES <xerxes@MacBook-Air-de-XERXES.local>
2025-09-04 20:02:30 +0200

    route of english page again

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <div className="absolute top-4 right-4 z-50">
         <Link
-          href="/en/page"
+          href="/en"
           className="bg-green-700 text-white px-4 py-2 rounded hover:bg-green-800 transition"
         >
           EN
